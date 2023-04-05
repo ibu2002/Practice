52c863f9072463266b016421b7c6c61be5584f12
Mohamed  Ibrahim <ibrahimthird7@gmail.com>
2023-04-05 12:34:01 +0530

    Adding comments to this code for more human readable and understanding

--- Reference.java
+++ Reference.java
@@ -1,12 +1,15 @@
 public class Reference {
     public static void main(String[] args) {
+
+        //creating the object to the class Address
         Address address = new Address();
         address.doorNo = "8/1";
         address.street = "Nazar Street, Periyar Nagar";
         address.city = "Avadi";
         address.state = "Tamilnadu";
         address.pincode = 600071;
-
+       
+       //Printing the information
         System.out.println(address.doorNo);
         System.out.println(address.street);
         System.out.println(address.city);
@@ -14,6 +17,9 @@ public class Reference {
         System.out.println(address.pincode);
     }
 }
+
+// creating the class name called Address
+
 class Address {
     String doorNo;
     String street;
